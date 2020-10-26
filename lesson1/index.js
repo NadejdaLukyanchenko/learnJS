541a324b5592423103252d1d588298c34d6aa2da
Lukyanchenko <nazydragnil33@gmail.com>
2020-10-26 21:31:48 +0300

    CreatFabrica

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,9 +1,9 @@
 class Game {
-    constructor(round, robot) {
-        this.round = 0;
-        this.robot1 = new Robot('Tulen');
-        this.robot2 = new Robot('Pingvin');
-        this.robot3 = new Robot('Dog')
+    constructor([robot1,robot2]) {
+        const fabric = new Robo_Fabrica();
+        this.robot1 = fabric.create(robot1.type,robot1.name);
+        this.robot2 = fabric.create(robot2.type,robot2.name);
+        this.round = 0; 
     }
 
     startGame() {
@@ -16,29 +16,23 @@ class Game {
     }
 
     __goRound() {
-        this.robot1.getUron(this.robot1.doUron());
-        if (!this.robot1.aLife()) {
-            console.log('Pingvin WIN!!!!')
+        this.robot2.getUron(this.robot1.doUron());
+        if (!this.robot2.aLife()) {
+            console.log(`${this.robot1.name} Win!`)
             return false
         } else {
-            this.robot2.getUron(this.robot1.doUron())
-            if (!this.robot2.aLife()) {
-                console.log('Tulen WIN!!!!')
+            this.robot1.getUron(this.robot2.doUron())
+            if (!this.robot1.aLife()) {
+                console.log(`${this.robot2.name} Win!`)
                 return false
-            }else{
-                this.robot3.getUron(this.robot2.doUron())
-                if (!this.robot3.aLife()) {
-                    console.log('Dog WIN!!!!')
-                    return false
-                }
-            }
+            } 
         }
         return true
     }
 }
 
 class Robot {
-    constructor(name,hp,uron) {
+    constructor(name, hp, uron) {
         this.hp = hp;
         this.uron = uron;
         this.name = name;
@@ -59,43 +53,64 @@ class Robot {
 
 class Heavy extends Robot {
     constructor(name) {
-        super(name,1000,100)
+        super(name, 1000, 100)
         this.armor = 20;
     }
 
     getUron(demage) {
-        this.hp = this.hp - (demage*(1-this.armor/100));
+        this.hp = this.hp - (demage * (1 - this.armor / 100));
     }
 };
 
 class Assault extends Robot {
     constructor(name) {
-        super(name,1000,100)
+        super(name, 1000, 100)
         this.cric = 50;
     }
 
     doUron() {
-        if(Math.random()*100>this.cric){
+        if (Math.random() * 100 > this.cric) {
             return this.uron
-        }else{
-            return this.uron*2
+        } else {
+            return this.uron * 2
         }
-        
+
     }
 };
 
 class Light extends Robot {
     constructor(name) {
-        super(name,1000,100)
+        super(name, 1000, 100)
         this.agility = 70;
-   }
+    }
 
     getUron(demage) {
-        if(Math.random()*100<this.agility){
-            this.hp=this.hp-demage
+        if (Math.random() * 100 < this.agility) {
+            this.hp = this.hp - demage
         }
     }
 };
-const robo = new Light ();
-robo.getUron(200)
-console.log(robo)
+
+class Robo_Fabrica {    
+    static type = {
+        h: Heavy,
+        a: Assault,
+        l: Light
+
+    }
+    create(type, name) {
+        const Robo = Robo_Fabrica.type[type]
+        const robo = new Robo(name)
+        return robo 
+    }
+
+}
+const robots = [{type:'a',name:'Tulen'},
+                {type:'h',name:'Pingvin'}
+];
+const g=new Game (robots)
+g.startGame()
+
+// const fabric = new Robo_Fabrica();
+// const my_robot=fabric.create('l','Tulen')
+// console.log(my_robot)
